877fb633fa758c63ec327048104986eabb7fd1e9
khalifaaa444 <116544385+khalifaaa444@users.noreply.github.com>
2023-08-31 18:34:01 +0300

    Add files via upload

---
+++ c_tasks/refactor_task/LCS_task/inc/lcs.h
@@ -0,0 +1,17 @@
+
+
+
+
+#ifndef _LCS_H_
+
+
+
+#define _LCS_H_
+
+#include "sort.h"
+
+
+int8_t lcsGetSize(int32_t *array, uint8_t arraySize, uint8_t *sizeofLCS);
+
+#endif
+
---
+++ c_tasks/refactor_task/LCS_task/inc/sort.h
@@ -0,0 +1,15 @@
+#ifndef  _SORT_H_
+
+#define _SORT_H_
+
+typedef unsigned char uint8_t ;
+typedef signed char int8_t ;
+typedef unsigned int uint32_t;
+typedef signed int int32_t;
+
+int8_t insertionSort(int32_t *array, uint8_t arraySize);
+void printArray(int32_t *array, uint8_t arraySize);
+
+
+
+#endif
